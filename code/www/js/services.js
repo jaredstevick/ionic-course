6e865c7d54c98788d0c7a5cefdc248ef29251f32
Jared Stevick <jstevick@gmail.com>
2015-06-19 16:57:58 -0400

    can now remove songs from favorites

--- code/www/js/services.js
+++ code/www/js/services.js
@@ -5,12 +5,23 @@ angular.module('songhop.services', [])
 	var o = {
 		favorites: []
 	}
+
+	// add songs to favorites
 	o.addSongToFavorites = function(song) {
 		// require a song to add
 		if (!song) return false;
 
 		//add it to the array
 		o.favorites.unshift(song);
+	}
+
+	// remove songs from favorites
+	o.removeSongFromFavorites = function(song, index) {
+		// check to see if there is song to add 
+		if (!song) return false;
+
+		// remove it
+		o.favorites.splice(index, 1);
 	}
 	return o;
 	
